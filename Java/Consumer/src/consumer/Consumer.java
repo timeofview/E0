540f61639a78215b72906c09fcc0dd220c4123c7
timeofview <neo>
2020-10-06 16:03:37 -0400

    new Consumer

--- Java/Consumer/src/consumer/Consumer.java
+++ Java/Consumer/src/consumer/Consumer.java
@@ -1,9 +1,25 @@
-package main;
+package consumer;
 
-public class Main {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Consumer {
 
 	public static void main(String[] args) {
-		System.out.println("f");
+		Character ignore = null;
+		int length = args.length;
+		try {
+			ignore = args[0].charAt(0);
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("Sintassi non corretta.");
+		}
+		for (int i = 1; i < length; i++) {
+			try {
+				new MyReader(new FileReader(args[i]), ignore).start();
+			} catch (FileNotFoundException e) {
+				System.out.println("Errore nell'apertura file "+args[i]);
+			}
+		}
 	}
 
 }
