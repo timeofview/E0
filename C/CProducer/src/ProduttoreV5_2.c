179cec41cc29649e48fabf3824389b51ce4abfd3
Scav <42140533+Scavix@users.noreply.github.com>
2020-10-07 14:36:53 +0200

    final Produttore

--- C/CProducer/src/ProduttoreV5_2.c
+++ C/CProducer/src/ProduttoreV5_2.c
@@ -6,16 +6,21 @@
 #define MAX_STRING_LENGHT 256
 #define DELIM ":"
 #define MAX_FILES 9
-/////produttore File1 File2 FileN
+
+
+//produttore File1 File2 FileN
 //NumFile         1     2    N
 //argv         argv[1]
 //fd              0     1     N-1
+//Formattazione Input> nLinea: messaggio
+
 int main(int argc, char* argv[]) {
     int n,trascritto, NumFiles;
     int fd[argc];
     char buffer[MAX_STRING_LENGHT];
     char *file_out[argc];
     char *token;
+
     //Controllo Parametri
     if (argc < 2 || argc>MAX_FILES+1){
         printf("SINTASSI ERRATA---> USO: NomeProgramma FileDoveScrivere \n oppure numero di file inserito troppo elevato (Max %d)", MAX_FILES);
@@ -23,50 +28,71 @@ int main(int argc, char* argv[]) {
     }
 
     NumFiles=argc-1;
+
+    //Setto i file descriptor a -1 per controllare successivamente se sono stati aperti
     for(n=0; n<NumFiles ; n++){
-        fd[n]=-1;   // setto i file descriptor a -1 per controllare successivamente se sono stati aperti
+        fd[n]=-1;   
     }
 
-        printf("Inserisci la nuova riga, (EOF per terminare) : \n");
-        gets(buffer);  // N  : riga
-    while(strcmp(buffer, "EOF")!=0 ){
-        
-        
+    //Primo Input 
+    printf("Inserisci la nuova riga, (EOF per terminare) : \n");
+
+    //Ciclo di Input con terminatore EOF
+    while( gets(buffer)!=NULL ){        
+
+	//Cut stringa
         token=strtok(buffer, DELIM);
+
+	//Check di Esistenza dell'indice
         if(token!=NULL){
-        n=atoi(token);
-        if(n > 0 && n <= NumFiles){
-            if(fd[n-1]==-1){ // se il file non è stato mai aperto
-                fd[n-1]= open(argv[n], O_CREAT | O_WRONLY | O_APPEND, 0777);
-                if(fd[n-1]<0){
-                    perror("IMPOSSIBILE APRIRE FILE-----terminazione del programma\n");
-                    exit(EXIT_FAILURE);
+	    
+	    //Conversione indice in Intero
+            n=atoi(token);
+
+	    //Check di validità dell'indice
+            if(n > 0 && n <= NumFiles){
+
+		//Se il file non è stato mai aperto
+                if(fd[n-1]==-1){
+                    fd[n-1]= open(argv[n], O_CREAT | O_WRONLY | O_APPEND, 0777);
+                    if(fd[n-1]<0){
+                        perror("IMPOSSIBILE APRIRE FILE-----terminazione del programma\n");
+                        exit(EXIT_FAILURE);
                 }
             }
+
+	    //Seleziono secondo token
             token = strtok(NULL, "\0");
-            token[strlen(token)+1] = "\0";
-            token[strlen(token)] = "\n";
+	   
+	    //Aggiungo terminatore stringa
+            token[strlen(token)+1] = '\0';
+
+	    //Aggiungo terminatore linea
+            token[strlen(token)] = '\n';
+
+	    //Scrittura
             trascritto = write(fd[n-1], token, strlen(token));
+
+	    //Check esito scrittura
             if(trascritto<0){
                 printf("Scrittura nel file %s non riuscita\n", argv[n]);
             }
-
-
-        }else{
-            printf("numero inserito non corretto oppure il carattere precedente i due punti non rappresenta un numero\n");
-        }}
-
-        printf("Inserisci la nuova riga, (EOF per terminare) : \n");
+        }
+	else{
+            printf("Numero inserito non corretto oppure il carattere precedente i due punti non rappresenta un numero\n");
+        }
+	}
+	//Chiedo input
+        printf("Inserisci la nuova riga, (EOF per terminare):\n");
     }
     
-
+    //Ciclo di chiusura
     for(n=0; n<argc; n++){
-        if(fd[n]!=-1){ // se il file è stato aperto allora lo chiudo;
+	//Se ho aperto il file allora lo chiudo
+        if(fd[n]!=-1){
             close(fd[n]);
         }
     }
-   
 
     return EXIT_SUCCESS;
 }
-    
