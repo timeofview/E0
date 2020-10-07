d2e9557a86cf8a2fcc37a1548ed48f979247a01f
Scav <42140533+Scavix@users.noreply.github.com>
2020-10-07 11:57:00 +0200

    Producer Java Final

--- Java/Producer/src/producer/Producer.java
+++ Java/Producer/src/producer/Producer.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 
 
 public class Producer {
@@ -13,10 +12,9 @@ public class Producer {
 		final int maxFileNumber = 9;
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String inputl = null;
-		ArrayList<String> righe = new ArrayList<String>();
 		String[] tmp;
 		int tmpValue;
-		FileWriter[] fout= new FileWriter[args.length];
+		FileWriter[] fout = new FileWriter[args.length];
 
         	//Check per 0 argomenti passati
         	if (args.length == 0 || args.length > maxFileNumber){
@@ -25,37 +23,9 @@ public class Producer {
 		}
 
 		//Check se il file esiste già
-		boolean exists[] = new boolean[args.length];
+		boolean[] exists = new boolean[args.length];
 		for(int i = 0; i < args.length; i++){
 			exists[i] = (Files.exists(Paths.get(args[i])))&&(!Files.isDirectory(Paths.get(args[i])));	
-		}
-
-		//Ciclo infinito in Input fino alla stringa "EOF" o riga vuota
-		while(true){
-			try{
-				//Leggo la riga
-				System.out.println("Inserisci <fileNumber>: <messaggi>(riga vuota o scrivi EOF per terminare):");
-				inputl = in.readLine();
-
-				//Check in ingresso per caratteri terminatori
-				if(inputl.equals("EOF")||inputl.isEmpty()){break;}
-				else{
-					//Aggiungo il newline char
-					inputl=inputl+"\n";
-					righe.add(inputl);
-				}
-			}
-			catch (IOException e) { 
-				e.printStackTrace();
-				System.exit(1); // uscita con errore, intero positivo a livello di sistema Unix
-			}
-		}
-
-		//Chiudo Input
-		try{in.close();}
-		catch (IOException e) { 
-			e.printStackTrace();
-			System.exit(2); // uscita con errore, intero positivo a livello di sistema Unix
 		}
 
 		//Ciclo di apertura dei FileWriter
@@ -72,38 +42,62 @@ public class Producer {
 			}
 			catch (IOException e) { 
 				e.printStackTrace();
-				System.exit(3); // uscita con errore, intero positivo a livello di sistema Unix
+				System.exit(1); // uscita con errore, intero positivo a livello di sistema Unix
 			}
 		}
 
-		//Ciclo di valutazione Input e Scrittura su file
-		for(String riga : righe){
+		//Ciclo infinito in Input fino alla stringa "EOF"
+		while(true){
 			try{
+				//Leggo la riga
+				System.out.println("Inserisci <fileNumber>: <messaggi>(EOF per terminare):");
+				inputl = in.readLine();
+
+				//Check in ingresso per carattere terminatore
+				if(inputl==null){
+					break;
+				}
+				else{
+					inputl+="\n";
+				}
+			}
+			catch (IOException e) { 
+				e.printStackTrace();
+				System.exit(2); // uscita con errore, intero positivo a livello di sistema Unix
+            }
+            try{
 				//Controlli sull'indice
-				tmp = riga.split(":");
+				tmp = inputl.split(":");
 				//Controllo sulla corretta impostazione della riga
 				if(tmp.length!=2){
-					System.out.println("Formato riga: <Filenumber> Stringa");
-					System.exit(4); // uscita con errore, intero positivo a livello di sistema Unix
+					System.out.println("Input invalido, Formato riga: <Filenumber> : Stringa ");
+					continue;
 				}
 				//Parse dell'intero
 				tmpValue = Integer.parseInt(tmp[0].trim());  
 				//Controlli sul valore
 				if(tmpValue<1||tmpValue>maxFileNumber||tmpValue>args.length){
-					System.out.println("Inserito un indice per un file non valido");
-					System.exit(5);
+					System.out.println("Inserito un indice per un file invalido");
+					System.exit(3); // uscita con errore, intero positivo a livello di sistema Unix
 				}
 				//Shifto Indietro di 1 perchè i file sono numerati da 1 a 9
 				fout[tmpValue-1].write(tmp[1], 0, tmp[1].length());
 			}
 			catch (NumberFormatException nfe) {
 				nfe.printStackTrace(); 
-				System.exit(6); // uscita con errore, intero positivo a livello di sistema Unix
+				System.exit(4); // uscita con errore, intero positivo a livello di sistema Unix
 			}
 			catch (IOException e) {
 				e.printStackTrace(); 
-				System.exit(7); // uscita con errore, intero positivo a livello di sistema Unix
+				System.exit(5); // uscita con errore, intero positivo a livello di sistema Unix
 			}
+		}
+
+		//Chiudo Input
+		try{in.close();}
+		catch (IOException e) { 
+			e.printStackTrace();
+			System.exit(6); // uscita con errore, intero positivo a livello di sistema Unix
 		}
 
 		//Ciclo di chiusura dei FileWriter
@@ -113,7 +107,7 @@ public class Producer {
 			}
 			catch (IOException e) { 
 				e.printStackTrace();
-				System.exit(8); // uscita con errore, intero positivo a livello di sistema Unix
+				System.exit(7); // uscita con errore, intero positivo a livello di sistema Unix
 			}
 		}
 	}
